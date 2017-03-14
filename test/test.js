a016af356ab1835f53e9af1c7c525767c06d2017
Koll Johnson <koll-johnson@openmailbox.org>
2017-03-13 21:37:30 -0500

    Refactor tests

--- test/test.js
+++ test/test.js
@@ -2,22 +2,20 @@ const assert = require('chai').assert;
 const VendingMachine = require('../index');
 
 describe('Vending Machine', () => {
+    let machine = new VendingMachine();
     
     describe('Accepts Coins', () => {
 	
 	it('should display INSERT COIN when no coins are inserted', () => {
-	    let machine = new VendingMachine();
 	    assert.equal('INSERT COIN', machine.display());
 	});
 
 	it('adds valid coin amount to current amount', () => {
-	    let machine = new VendingMachine();
 	    machine.insertCoin('quarter');
 	    assert.equal('0.25', machine.display());
 	});
 
 	it('rejects invalid coins', () => {
-	    let machine = new VendingMachine();
 	    machine.insertCoin('penny');
 	    assert.lengthOf(machine.coinReturn(), 1);
 	});
