69c0f2f28d30ab8d8481b256de77357434d938c2
Koll Johnson <koll-johnson@openmailbox.org>
2017-03-13 21:01:15 -0500

    Implement ValidCoin

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 
 function VendingMachine() {}
 
+let validCoins = {'quarter': 0.25}
+
+VendingMachine.prototype.currentAmount = 0;
+
 VendingMachine.prototype.display = function () {
-    return 'INSERT COIN';
+    if(VendingMachine.prototype.currentAmount > 0) {
+	return VendingMachine.prototype.currentAmount.toString();
+    } else {
+	return 'INSERT COIN';
+    }
+}
+
+VendingMachine.prototype.insertCoin = function (coin) {
+    let amount = checkCoin(coin);
+    VendingMachine.prototype.currentAmount += amount;
+    VendingMachine.prototype.currentAmount.toFixed(2);
+}
+
+function checkCoin(coin) {
+    if(coin in validCoins) {
+	return validCoins[coin];
+    }
 }
 
 module.exports = VendingMachine;
