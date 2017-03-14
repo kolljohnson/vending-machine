bf75793720620e07cd22f1cb1570bf5adc162bcc
Koll Johnson <koll-johnson@openmailbox.org>
2017-03-13 22:00:37 -0500

    Implement DisplayPrice

--- index.js
+++ index.js
@@ -2,14 +2,20 @@
 function VendingMachine() {}
 
 let validCoins = {'nickel': 0.05, 'dime': 0.10, 'quarter': 0.25};
+let products = {'cola': 1.00, 'chips': 0.50, 'candy': 0.65}
 let currentAmount = 0;
+let priceDisplay = '';
 let coinReturn = [];
 
 VendingMachine.prototype.display = () => {
-    if(currentAmount > 0) {
-	return currentAmount.toString();
+    if(priceDisplay != '') {
+	return priceDisplay;
     } else {
-	return 'INSERT COIN';
+	if(currentAmount > 0) {
+	    return currentAmount.toString();
+	} else {
+	    return 'INSERT COIN';
+	}
     }
 }
 
@@ -25,6 +31,11 @@ VendingMachine.prototype.insertCoin = (coin) => {
 
 VendingMachine.prototype.coinReturn = () => {
     return coinReturn;
+}
+
+VendingMachine.prototype.pressButton = (button) => {
+    let price = products[button];
+    priceDisplay = `PRICE ${price.toFixed(2)}`;
 }
 
 function checkCoin(coin) {
