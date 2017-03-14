8107b90f770110947b7649c1d561087467e7891d
Koll Johnson <koll-johnson@openmailbox.org>
2017-03-14 18:48:40 -0500

    Implement MakeChange

--- index.js
+++ index.js
@@ -1,49 +1,72 @@
+let validCoins = {'quarter': 0.25,'dime': 0.10, 'nickel': 0.05};
+let products = {'cola': 1.00, 'chips': 0.50, 'candy': 0.65};
 
-function VendingMachine() {}
+function VendingMachine() {
+    let coinReturn = [];
+    let currentAmount = 0;
+    this.priceDisplay = '';
+    this.purchaseComplete = false;
 
-let validCoins = {'nickel': 0.05, 'dime': 0.10, 'quarter': 0.25};
-let products = {'cola': 1.00, 'chips': 0.50, 'candy': 0.65}
-let currentAmount = 0;
-let priceDisplay = '';
-let coinReturn = [];
-let purchaseComplete = false;
-
-VendingMachine.prototype.display = () => {
-    let display = 'INSERT COIN'
-    if(purchaseComplete) {
-	purchaseComplete = false;
+    this.display = () => {
+	let display = 'INSERT COIN'
+	if(this.purchaseComplete) {
+	    this.purchaseComplete = false;
+	}
+	else if(this.priceDisplay != '') {
+	    this.checkPriceDisplay();
+	    display = this.priceDisplay;
+	} else if (currentAmount > 0) {
+	    display = `$${currentAmount.toString()}`;
+	}
+	return display;
     }
-    else if(priceDisplay != '') {
-	checkPriceDisplay();
-	display = priceDisplay;
-    } else if (currentAmount > 0) {
-	display = `$${currentAmount.toString()}`;
-    }
-    return display;
-}
 
-VendingMachine.prototype.insertCoin = (coin) => {
-    if(checkCoin(coin) > 0) {
-	let amount = checkCoin(coin);
-	currentAmount += amount;
-	currentAmount.toFixed(2);
+    this.insertCoin = (coin) => {
+	if(checkCoin(coin) > 0) {
+	    let amount = checkCoin(coin);
+	    currentAmount += amount;
+	    currentAmount.toFixed(2);
+	} else {
+	    coinReturn.push(coin);
+	}
+    }
+    
+    this.getCoins = () => {
+	return coinReturn;
+    }
+
+    this.pressButton = (button) => {
+	let price = products[button];
+	if(price <= currentAmount) {
+	    this.priceDisplay = 'THANK YOU';
+	    currentAmount -= price;
+	    currentAmount = (Math.round(currentAmount * Math.pow(10,2))/Math.pow(10,2)).toFixed(2);
+	if(currentAmount > 0) {
+	    this.makeChange();
+	}
     } else {
-	coinReturn.push(coin);
+	this.priceDisplay = `PRICE $${price.toFixed(2)}`;
+    }
+    }
+
+    this.checkPriceDisplay = () => {
+	if(this.priceDisplay == 'THANK YOU') {
+	    this.purchaseComplete = true;
+	    currentAmount = 0;
+	}
+    }
+
+    this.makeChange = () => {
+	Object.keys(validCoins).forEach(function(key, index) {
+	    console.log(currentAmount);
+	    if(currentAmount >= validCoins[key].toFixed(2)) {
+		currentAmount -= validCoins[key];
+		coinReturn.push(key);
+	    }
+	});
     }
 }
 
-VendingMachine.prototype.coinReturn = () => {
-    return coinReturn;
-}
-
-VendingMachine.prototype.pressButton = (button) => {
-    let price = products[button];
-    if(price <= currentAmount) {
-	priceDisplay = 'THANK YOU';
-    } else {
-	priceDisplay = `PRICE $${price.toFixed(2)}`;
-    }
-}
 
 function checkCoin(coin) {
     if(coin in validCoins) {
@@ -52,11 +75,5 @@ function checkCoin(coin) {
     return 0;
 }
 
-function checkPriceDisplay() {
-    if(priceDisplay == 'THANK YOU') {
-	purchaseComplete = true;
-	currentAmount = 0;
-    }
-}
 
 module.exports = VendingMachine;
