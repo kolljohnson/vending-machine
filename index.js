a5c3beeaf824d34dd2cbc37cf9d75d8490030d4f
Koll Johnson <koll-johnson@openmailbox.org>
2017-03-13 21:08:17 -0500

    Refactor of ValidCoin

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 
 function VendingMachine() {}
 
-let validCoins = {'quarter': 0.25}
+let validCoins = {'nickel': 0.05, 'dime': 0.10, 'quarter': 0.25};
+let currentAmount = 0;
 
-VendingMachine.prototype.currentAmount = 0;
-
-VendingMachine.prototype.display = function () {
-    if(VendingMachine.prototype.currentAmount > 0) {
-	return VendingMachine.prototype.currentAmount.toString();
+VendingMachine.prototype.display = () => {
+    if(currentAmount > 0) {
+	return currentAmount.toString();
     } else {
 	return 'INSERT COIN';
     }
 }
 
-VendingMachine.prototype.insertCoin = function (coin) {
+VendingMachine.prototype.insertCoin = (coin) => {
     let amount = checkCoin(coin);
-    VendingMachine.prototype.currentAmount += amount;
-    VendingMachine.prototype.currentAmount.toFixed(2);
+    currentAmount += amount;
+    currentAmount.toFixed(2);
 }
 
 function checkCoin(coin) {
