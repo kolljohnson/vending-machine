f51a4a7e0505917166626f7be03dd50ed48dcf19
Koll Johnson <koll-johnson@openmailbox.org>
2017-03-13 21:34:59 -0500

    Implement InvalidCoin

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function VendingMachine() {}
 
 let validCoins = {'nickel': 0.05, 'dime': 0.10, 'quarter': 0.25};
 let currentAmount = 0;
+let coinReturn = [];
 
 VendingMachine.prototype.display = () => {
     if(currentAmount > 0) {
@@ -13,15 +14,24 @@ VendingMachine.prototype.display = () => {
 }
 
 VendingMachine.prototype.insertCoin = (coin) => {
-    let amount = checkCoin(coin);
-    currentAmount += amount;
-    currentAmount.toFixed(2);
+    if(checkCoin(coin) > 0) {
+	let amount = checkCoin(coin);
+	currentAmount += amount;
+	currentAmount.toFixed(2);
+    } else {
+	coinReturn.push(coin);
+    }
+}
+
+VendingMachine.prototype.coinReturn = () => {
+    return coinReturn;
 }
 
 function checkCoin(coin) {
     if(coin in validCoins) {
 	return validCoins[coin];
     }
+    return 0;
 }
 
 module.exports = VendingMachine;
