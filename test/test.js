d1594a2f64d84bd7b69b0d0db9b978dc375867ba
Koll Johnson <koll-johnson@openmailbox.org>
2017-03-13 19:54:08 -0500

    Small refactor of Insert Coin Test

--- test/test.js
+++ test/test.js
@@ -1,11 +1,12 @@
-let assert = require('assert');
-require('../index.js');
+const assert = require('assert');
+const VendingMachine = require('../index');
 
 describe('Vending Machine', () => {
     describe('Accepts Coins', () => {
+	
 	it('should display INSERT COIN when no coins are inserted', () => {
 	    let machine = new VendingMachine();
-	    assert.equal('INSERT COIN', machine.display);
+	    assert.equal('INSERT COIN', machine.display());
 	});
     });
 });
